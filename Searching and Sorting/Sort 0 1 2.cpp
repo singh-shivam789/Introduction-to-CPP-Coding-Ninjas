0642d80e53e4e1fd5ada01cec90507984179f52d
Shivam Singh <52860981+singh-shivam789@users.noreply.github.com>
2020-11-09 23:32:39 +0530

    code better solution

--- Searching and Sorting/Sort 0 1 2.cpp
+++ Searching and Sorting/Sort 0 1 2.cpp
@@ -1,28 +1,24 @@
 // Sort 0 1 2
-// You are given an integer array/list(ARR) of size N. It contains only 0s, 1s and 2s. 
+// You are given an integer array/list(ARR) of size N. It contains only 0s, 1s and 2s.
 // Write a solution to sort this array/list in a 'single scan'.
-// 'Single Scan' refers to iterating over the array/list just once or to put it in other words, 
+// 'Single Scan' refers to iterating over the array/list just once or to put it in other words,
 // you will be visiting each element in the array/list just once.
+// In case you didn’t know, this problem is a variation of a famous problem called the ‘Dutch National Flag Problem’
 
 #include<bits/stdc++.h>
 using namespace std;
-
 void sort012(int *arr, int n){
-    int temp[n];
-    int k,i,j;
-    for(i=0, j=n-1, k=0; k<n; k++){
-        if(arr[k] == 0){
-            temp[i++] = 0;
+    int i=0,nz=0,nt=n-1;
+    while(i<=nt){
+        if(arr[i] == 0){
+            swap(arr[i++], arr[nz++]);
         }
-        else if(arr[k] == 2){
-            temp[j--] = 2;
+        else if(arr[i] == 1){
+            i++;
         }
-    }
-    for(; i<=j; i++){
-        temp[i] = 1;
-    }
-    for(i=0; i<n; i++){
-        arr[i] = temp[i];
+        else{
+            swap(arr[i], arr[nt--]);
+        }
     }
     return;
 }
